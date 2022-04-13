1d47c30e442ff88cddd117358d07f68869137f44
Nicolò Piccolo <nicopico77@gmail.com>
2022-04-13 21:42:33 +0200

    Fetch Call from FakeStore API

--- 23_mar_2022/main.js
+++ 23_mar_2022/main.js
@@ -1,15 +1,25 @@
 import { createCard, getAPIdata, deleteCard, formatMinText } from "./utils.js";
 
 getAPIdata("https://fakestoreapi.com/products").then((data) => {
+  
   data.map((product) => {
+  
     createCard(
+  
       formatMinText(product.title),
+  
       formatMinText(product.description),
+  
       product.image,
+  
       "€ " + product.price 
+  
     )})
+
     const cardSelector = document.querySelectorAll(".card")
+
     cardSelector.forEach((card) => deleteCard(card));
+
 });
 
 
--- 23_mar_2022/utils.js
+++ 23_mar_2022/utils.js
@@ -1,42 +1,63 @@
 const createCard = (title, desc, imgUrl, price) => {
-    const divEl = document.createElement("div");
-    const h3El = document.createElement("h3");
-    const parDescEl = document.createElement("p");
-    const imgEl = document.createElement("img");
-    const parPriceEl = document.createElement("p");
+    
+  const divEl = document.createElement("div");
   
-    divEl.classList.add("card");
-    parPriceEl.classList.add("price");
-    parDescEl.classList.add("description");
+  const h3El = document.createElement("h3");
   
-    imgEl.setAttribute("src", imgUrl);
-    imgEl.setAttribute("alt", "img cosi");
-    h3El.textContent = title;
-    parDescEl.textContent = desc;
-    parPriceEl.textContent = price;
+  const parDescEl = document.createElement("p");
   
-    divEl.append(h3El, parDescEl, imgEl, parPriceEl);
-    document.querySelector(".cards-wrapper").appendChild(divEl);
-  };
+  const imgEl = document.createElement("img");
+  
+  const parPriceEl = document.createElement("p");
+  
+  
+  divEl.classList.add("card");
+  
+  parPriceEl.classList.add("price");
+  
+  parDescEl.classList.add("description");
+  
+  
+  imgEl.setAttribute("src", imgUrl);
+  
+  imgEl.setAttribute("alt", "img cosi");
+  
+  h3El.textContent = title;
+  
+  parDescEl.textContent = desc;
+  
+  parPriceEl.textContent = price;
+  
+  divEl.append(h3El, parDescEl, imgEl, parPriceEl);
+  
+  document.querySelector(".cards-wrapper").appendChild(divEl);
+  
+};
 
   const getAPIdata = async (URL, item = "") => {
+    
     const res = await fetch(`${URL}${item}`);
   
+    
     return await res.json();
+  
   };
 
 
 const deleteCard = (element) => {
-    const wrapperSel = document.querySelector(".cards-wrapper");
-   element.addEventListener("click", () => { 
-       console.log();
-      
-       wrapperSel.removeChild(element);
+  
+  const wrapperSel = document.querySelector(".cards-wrapper");
+  
+  element.addEventListener("click", () => { 
+  
+    wrapperSel.removeChild(element);
        
-   });  
+  });  
+
 };
 
-const formatMaxText = (text) => text.split(".")[0];
+
+//const formatMaxText = (text) => text.split(".")[0];
 
 const formatMinText = (text) => text.split(" ").slice(0, 6).join(" ") + "...";
 
