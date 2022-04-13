6b42ddb2194b94a2e8ed27d72b2cfafa94195045
Nicolò Piccolo <nicopico77@gmail.com>
2022-04-13 20:07:52 +0200

    Arithmetic average Prompt Calculator

--- 9_mar_2022/script.js
+++ 9_mar_2022/script.js
@@ -2,7 +2,9 @@
 //ARRAY
 let nInput =  [3, 5, 10, 2, 8];
 */
+
 let sum = 0;
+
 /*
 //CICLO SOMMA ELEMENTI ARRAY nINPUT
 for (let i=0; i < nInput.length; i++) {
@@ -18,20 +20,26 @@ console.log("la media è:", media)
 alert( "la media è : " + media);
 */
 
-/*USER INPUT*/
+//USER INPUT
 
 let nInput = [];
 
 //CICLO MEDIA USER nINPUT
-for (i=0; i < 4; i++)
-{
+
+for (i=0; i < 4; i++) {
+
     nInput.push(+prompt("Digita un numero" ))
+    
     sum+= nInput[i];
+
 }
 
 // LOG nINPUT, MEDIA
+
 console.log(nInput)
+
 let media = sum / nInput.length ;
+
 console.log("la media è:", media);
 
 //OUTPUT
